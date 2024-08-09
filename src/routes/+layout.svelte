fe7684aeea44a6f7c949c207cfdfcedccc956d02
Erston Greatman <erston.greatman@gmail.com>
2024-08-08 22:28:45 -0600

    Minor Name Change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar><h1 class="h1">GTA Shop</h1></AppBar>
+		<AppBar><h1 class="h1">Stellar Shop</h1></AppBar>
 	</svelte:fragment>
 
 	<article class="container flex flex-col gap-4 items-center justify-center p-8">
